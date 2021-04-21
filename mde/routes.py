b15de4dc657bcde379e5d3f80051892caebe2a4b
Iris Leal <iris_leal_m@hotmail.com>
2021-04-21 10:03:02 -0700

    Added error page and route.

--- mde/routes.py
+++ mde/routes.py
@@ -15,3 +15,13 @@ def home_page():
 @app.route('/login')
 def login_page():
     return render_template('login.html')
+
+
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # Note the 404 after the render_template() call. This tells Flask that the status code of 
+    # that page should be 404 which means not found. By default 200 is assumed which 
+    # translates to: all went well.
+    return render_template('error.html', error=error), 404
