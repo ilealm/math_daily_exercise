50cae607ed96e665a6ab965adcb465c51e77e6c0
Iris Leal <iris_leal_m@hotmail.com>
2021-05-06 16:03:33 -0700

    Ready layout redesing on game and play

--- mde/forms.py
+++ mde/forms.py
@@ -58,7 +58,7 @@ class PlayForm(FlaskForm):
         DataRequired(message='The amount is required.'),
         NumberRange(
             min=1, max=30, message='The amount per game must be an integer between 1 and 30.')
-    ], default=3)
+    ], default=20)
     mode = RadioField(u'Mode', choices=PLAY_MODES, default=1, validators=[
         # in order to display this message, this MUST BE InputRequired, NOT DataRequired.
         InputRequired(message="Please select a practice mode.")])
