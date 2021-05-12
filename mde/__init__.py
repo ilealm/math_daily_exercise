36a742ecfc97efa059efd8723d5e8a3e902232e6
Iris Leal <iris_leal_m@hotmail.com>
2021-05-12 13:31:03 -0700

    base config

--- mde/__init__.py
+++ mde/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-# with this, I import the config file
+# # with this, I import the config file
 app.config.from_pyfile('config.py')
+
+
+
 
 db = SQLAlchemy(app)
 
--- mde/config.py
+++ mde/config.py
@@ -2,9 +2,20 @@ from os import environ, path
 # I need to install python-dotenv to use this file
 from dotenv import load_dotenv
 
+
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
-# DB config
-SQLALCHEMY_DATABASE_URI = 'sqlite:///mde.db'
+
+
+# base config
+FLASK_ENV = 'development'
 SECRET_KEY = environ.get('SECRET_KEY')
+STATIC_FOLDER = 'static'
+TEMPLATES_FOLDER = 'templates'
+DEBUG = True
+TESTING = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///mde.db'
+
+
+
